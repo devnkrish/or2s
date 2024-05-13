bc7f50e49ddfecca1e47264cf7566aae776da14b
devnkrish <nkrishnendu101@gmail.com>
2024-05-13 13:28:21 +0530

    Update footer.php

--- User/footer.php
+++ User/footer.php
@@ -11,13 +11,6 @@
             </h1>
 
             <div class="footer-bottom">
-
-
-
-
-
-
-
                 <a target="_blank" rel="noopener" href="/or2s/User/images/footerimage/fresher.jpg">
                     <img src="/or2s/User/images/footerimage/sponsor.png" width="300" height="50" alt="My Image">
                 </a>
@@ -29,7 +22,7 @@
     <div id="last">
 
         <div class="footer-bottom">
-            <p>OR2S | &copy; 2023 | All Rights Reserved | Developed by Rakesh Mondal
+            <p>OR2S | &copy; 2023 | All Rights Reserved | Developed by nCoder
                 <a target="_blank" rel="noopener" href="https://www.facebook.com/imrk.859?mibextid=LQQJ4d"><img
                         src="/or2s/User/images/footerimage/facebook.png" width="20" height="20"></a>
                 <a target="_blank" rel="noopener" href="https://instagram.com/rak3xh?igshid=YmMyMTA2M2Y="><img
